4bc10d3013860fe600fc757e147167b087f5b5fa
dustin <dustinmcdowell1989@gmail.com>
2016-05-24 20:37:25 -0600

    added styling and reset button

--- todo.js
+++ todo.js
@@ -2,11 +2,12 @@
 
 function init() {
 	var input = document.querySelector("input");
-	$("button").on("click",function() {
+	$("#add").on("click",function() {
 		if (input.value !== "") {
 			$("ul").append("<li>" + $("input").val() + "</li>");
 			$("input").val("");
 			crossOut();
+			reset();
 		}
 
 	});
@@ -15,6 +16,12 @@ function init() {
 function crossOut() {
 	$("li").on("click", function() {
 		$(this).addClass("cross-out");
+	});
+};
+
+function reset() {
+	$("#reset").on("click", function() {
+		$("ul").html(null);
 	});
 };
 
